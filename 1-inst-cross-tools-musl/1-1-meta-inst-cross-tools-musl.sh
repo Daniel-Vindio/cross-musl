1697b29b114404388d1401d3c5927290c59b1d3b
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2018-12-23 17:16:32 +0100

    Add files via upload

--- 1-inst-cross-tools-musl/1-1-meta-inst-cross-tools-musl.sh
+++ 1-inst-cross-tools-musl/1-1-meta-inst-cross-tools-musl.sh
@@ -11,20 +11,21 @@ echo -e "
 #$0\n\
 ############################################################\n"
 
-./icrosstmusl1_file.sh 		$VER_file 	gz
-./icrosstmusl2_linux.sh 	$VER_linux 	xz
-./icrosstmusl3_m4.sh 		$VER_m4 	xz
-./icrosstmusl4_ncurses.sh 	$VER_ncurses gz
-./icrosstmusl5_pkg-config-lite.sh $VER_pkg gz
-./icrosstmusl6_gmp.sh 		$VER_gmp 	xz
-./icrosstmusl7_mpfr.sh 		$VER_mpfr 	xz
-./icrosstmusl8_mpc.sh 		$VER_mpc 	gz
-./icrosstmusl44_binutils.sh $VER_binutils gz
-./icrosstmusl45_gcc.sh		$VER_gcc	gz
-./icrosstmusl46_musl.sh		$VER_musl	gz
-cp -v $srcinst1/icrosstmusl_limits.h $CLFS/tools/include/limits.h
-./icrosstmusl47_gcc.sh		$VER_gcc	gz
+./icrosstmusl1_file.sh 				$VER_file 	gz
+./icrosstmusl2_linux.sh 			$VER_linux 	xz
+./icrosstmusl3_m4.sh 				$VER_m4 	xz
+./icrosstmusl4_ncurses.sh 			$VER_ncurses gz
+./icrosstmusl5_pkg-config-lite.sh 	$VER_pkg 	gz
+./icrosstmusl6_gmp.sh 				$VER_gmp 	xz
+./icrosstmusl7_mpfr.sh 				$VER_mpfr 	xz
+./icrosstmusl8_mpc.sh 				$VER_mpc 	gz
+./icrosstmusl44_binutils.sh 		$VER_binutils gz
+./icrosstmusl45_gcc.sh				$VER_gcc	gz
+./icrosstmusl46_musl.sh				$VER_musl	gz
 
+cp -v $srcinst1/icrosstmusl_limits.h $CLFS/tools/include/limits.h
+
+./icrosstmusl47_gcc.sh				$VER_gcc	gz
 
 echo -e "
 ############################################################\n\
--- 1-inst-cross-tools-musl/1-2-meta-inst-tmp-sys-musl.sh
+++ 1-inst-cross-tools-musl/1-2-meta-inst-tmp-sys-musl.sh
@@ -5,13 +5,13 @@ echo -e "
 #$0\n\
 ############################################################\n"
 
-./itmpsysmusl16_gmp.sh 		$VER_gmp 		xz
+./itmpsysmusl16_gmp.sh 			$VER_gmp 		xz
 ./itmpsysmusl17_mpfr.sh 		$VER_mpfr 		xz
-./itmpsysmusl18_mpc.sh 		$VER_mpc 		gz
-./itmpsysmusl21_zlib.sh		$VER_zlib		xz
-./itmpsysmusl22_binutils.sh	$VER_binutils 	gz
+./itmpsysmusl18_mpc.sh 			$VER_mpc 		gz
+./itmpsysmusl21_zlib.sh			$VER_zlib		xz
+./itmpsysmusl22_binutils.sh		$VER_binutils 	gz
 ./itmpsysmusl23_gcc.sh 			$VER_gcc		gz
-./itmpsysmusl24_ncurses.sh 	$VER_ncurses 	gz
+./itmpsysmusl24_ncurses.sh 		$VER_ncurses 	gz
 ./itmpsysmusl25_bash.sh 		$VER_bash		gz
 ./itmpsysmusl26_bzip2.sh 		$VER_bzip2 		gz
 ./itmpsysmusl27_check.sh 		$VER_check 		gz
@@ -20,17 +20,17 @@ echo -e "
 ./itmpsysmusl30_file.sh 		$VER_file 		gz
 ./itmpsysmusl31_findutils.sh	$VER_findutils	gz
 ./itmpsysmusl32_gawk.sh 		$VER_gawk 		xz
-./itmpsysmusl33_gettext.sh 	$VER_gettext 	xz
+./itmpsysmusl33_gettext.sh 		$VER_gettext 	xz
 ./itmpsysmusl34_grep.sh 		$VER_grep 		xz
 ./itmpsysmusl35_gzip.sh 		$VER_gzip	 	xz
 ./itmpsysmusl36_make.sh 		$VER_make 		bz2
 ./itmpsysmusl37_patch.sh 		$VER_patch 		xz
-./itmpsysmusl38_sed.sh 		$VER_sed 		xz
-./itmpsysmusl39_tar.sh 		$VER_tar 		xz
+./itmpsysmusl38_sed.sh 			$VER_sed 		xz
+./itmpsysmusl39_tar.sh 			$VER_tar 		xz
 ###./itmpsysmusl40_texinfo.sh 	$VER_texinfo 	xz	#Impossible to install now
 ./itmpsysmusl41_util-linux.sh	$VER_util_linux xz
 ###./itmpsysmusl42_vim.sh 		$VER_vim 		bz2	#Impossible to install now
-./itmpsysmusl43_xz.sh 		$VER_xz 		xz
+./itmpsysmusl43_xz.sh 			$VER_xz 		xz
 
 
 echo -e "
