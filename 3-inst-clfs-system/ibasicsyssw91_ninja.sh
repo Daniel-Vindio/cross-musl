79228c9db8148fb8d7c8e47ed39b0238285af9f5
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2019-01-05 08:20:28 +0100

    Add files via upload

---
+++ 3-inst-clfs-system/ibasicsyssw91_ninja.sh
@@ -0,0 +1,110 @@
+# Instalador de ninja
+# MUSL.
+
+#ninja-1.8.2.tar.gz
+
+nombre=$(echo $0 | cut -d "." -f2 | cut -d "_" -f2)
+nombre_comp=$nombre-$1.tar.$2
+nombre_dir=$nombre-$1
+
+T_COMIENZO=$(date +"%T")	#para calcular el tiempo de instalación
+
+function registro_error () {
+#Función para registrar errores y resultados en la bitaćora
+if [ $? -ne 0 ]
+then
+	MOMENTO=$(date)
+	echo $MSG_ERR
+	echo "$MOMENTO $nombre <$1> -> ERROR" >> $FILE_BITACORA
+	exit 1
+else
+	MOMENTO=$(date)
+	echo "$MOMENTO $nombre <$1> -> Conforme" >> $FILE_BITACORA
+fi
+}
+
+
+# $# es el nº de argumentos. Aquí se comprueba la sintaxis de la 
+# aplicación
+if [ $# != 2 ]
+then
+	echo "uso ./nombre vesion compresion"
+	echo "nombre = nombre del programa"
+	echo "version = x.y.z"
+	echo "compresión = gz / xz etc"
+	exit 1
+fi
+
+if [ $(id -u) -ne 0 ]
+	then 
+		echo -e "Ur not root bro"
+		echo -e "Tines que ser root, tío"
+	exit 1
+fi
+
+cd $DIR_FUENTES
+
+if [ -e $nombre_comp ]
+then
+	echo "Comienza la descompresión ..."
+else
+	echo "no existe el archivo, o no tiene el formato .tar"
+	echo "se abandona la instalación"
+	exit 1
+fi
+
+case $2 in
+	gz)
+	tar -zxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	bz2)
+	tar -jxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	xz)
+	tar -xf $nombre_comp || (echo "error descompresión"; exit 1);;
+	
+	tgz)
+	tar -xvzf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	*)
+	echo "uso ./$nombre x.y.z (gz/bz2/zx)"
+	exit 1;;
+esac	
+echo "... fin de la descompresión"
+
+cd $nombre_dir
+
+#----------------------CONFIGURE - MAKE - MAKE INSTALL------------------
+
+echo -e "\nInstalacion de $nombre_dir MUSL" >> $FILE_BITACORA
+
+python3 configure.py --bootstrap
+registro_error $MSG_CONF
+
+python3 configure.py
+registro_error "Configure 2"
+
+./ninja ninja_test
+registro_error "Test 1"
+
+./ninja_test --gtest_filter=-SubprocessTest.SetWithLots
+registro_error "Test 2"
+
+install -vm755 ninja /usr/bin/
+registro_error "install 1"
+
+install -vDm644 misc/bash-completion /usr/share/bash-completion/completions/ninja
+registro_error "install 2"
+
+install -vDm644 misc/zsh-completion /usr/share/zsh/site-functions/_ninja
+registro_error "install 3"
+
+######------------------------------------------------------------------
+
+cd ..
+rm -rf $nombre_dir && echo "Borrado el directorio $nombre_dir"
+
+
+#Registro de tiempos de ejecución
+T_FINAL=$(date +"%T")
+echo "$(date) $nombre <$MSG_TIME> $T_COMIENZO $T_FINAL" >> $FILE_BITACORA
