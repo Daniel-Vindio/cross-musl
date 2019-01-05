3c36f133e96e528e80e1bd4a4522fd0649165e91
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2019-01-05 08:43:02 +0100

    Add files via upload

---
+++ 3-inst-clfs-system/ibasicsyssw93_procps-ng.sh
@@ -0,0 +1,114 @@
+# Instalador de procps-ng
+# MUSL.
+
+#procps-ng-3.3.12.tar.xz
+
+nombre="procps-ng"
+nombre_comp=$nombre-$1.tar.$2
+nombre_dir=$nombre-$1
+
+T_COMIENZO=$(date +"%T")	#para calcular el tiempo de instalación
+
+function registro_error () {
+#Función para registrar errores y resultados en la bitaćora
+if [ $? -ne 0 ]
+then
+	MOMENTO=$(date)
+	echo $MSG_ERR
+	echo "$MOMENTO $nombre <$1> -> ERROR" >> $FILE_BITACORA
+	exit 1
+else
+	MOMENTO=$(date)
+	echo "$MOMENTO $nombre <$1> -> Conforme" >> $FILE_BITACORA
+fi
+}
+
+
+# $# es el nº de argumentos. Aquí se comprueba la sintaxis de la 
+# aplicación
+if [ $# != 2 ]
+then
+	echo "uso ./nombre vesion compresion"
+	echo "nombre = nombre del programa"
+	echo "version = x.y.z"
+	echo "compresión = gz / xz etc"
+	exit 1
+fi
+
+if [ $(id -u) -ne 0 ]
+	then 
+		echo -e "Ur not root bro"
+		echo -e "Tines que ser root, tío"
+	exit 1
+fi
+
+cd $DIR_FUENTES
+
+if [ -e $nombre_comp ]
+then
+	echo "Comienza la descompresión ..."
+else
+	echo "no existe el archivo, o no tiene el formato .tar"
+	echo "se abandona la instalación"
+	exit 1
+fi
+
+case $2 in
+	gz)
+	tar -zxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	bz2)
+	tar -jxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	xz)
+	tar -xf $nombre_comp || (echo "error descompresión"; exit 1);;
+	
+	tgz)
+	tar -xvzf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	*)
+	echo "uso ./$nombre x.y.z (gz/bz2/zx)"
+	exit 1;;
+esac	
+echo "... fin de la descompresión"
+
+cd $nombre_dir
+
+#----------------------CONFIGURE - MAKE - MAKE INSTALL------------------
+
+echo -e "\nInstalacion de $nombre_dir MUSL" >> $FILE_BITACORA
+
+./configure \
+--prefix=/usr \
+--exec-prefix= \
+--libdir=/usr/lib \
+--disable-static \
+--disable-kill
+registro_error $MSG_CONF
+
+make
+registro_error $MSG_MAKE
+
+sed -i -r 's|(pmap_initname)\\\$|\1|' testsuite/pmap.test/pmap.exp
+sed -i '/set tty/d' testsuite/pkill.test/pkill.exp
+rm testsuite/pgrep.test/pgrep.exp
+#make check 2>&1 | tee $FILE_CHECKS
+
+make install
+registro_error $MSG_INST
+
+mv -v /usr/lib/libprocps.so.* /lib
+registro_error "mv"
+
+ln -sfv ../../lib/$(readlink /usr/lib/libprocps.so) /usr/lib/libprocps.so
+registro_error "ln -sfv ../"
+
+######------------------------------------------------------------------
+
+cd ..
+rm -rf $nombre_dir && echo "Borrado el directorio $nombre_dir"
+
+
+#Registro de tiempos de ejecución
+T_FINAL=$(date +"%T")
+echo "$(date) $nombre <$MSG_TIME> $T_COMIENZO $T_FINAL" >> $FILE_BITACORA
