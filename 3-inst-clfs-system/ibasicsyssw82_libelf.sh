2699db48c8ba69ce651d3909d7a43bb470bf7bf8
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2019-01-06 22:12:28 +0100

    Add files via upload

--- 3-inst-clfs-system/ibasicsyssw82_libelf.sh
+++ 3-inst-clfs-system/ibasicsyssw82_libelf.sh
@@ -77,14 +77,12 @@ cd $nombre_dir
 
 echo -e "\nInstalacion de libelf $nombre_dir MUSL" >> $FILE_BITACORA
 
+patch -Np1 -i ../elfutils-0.174.patch
+registro_error "parche"
+
 ./configure \
 --prefix=/usr
 registro_error $MSG_CONF
-
-#Texto1="CFLAGS = -D_FORTIFY_SOURCE=2 -g -O2"
-#Texto2="CFLAGS = -D_FORTIFY_SOURCE=2 -g -O2 -Wno-error=packed-not-aligned"
-#sed -i 's/'"$Texto1"'/'"$Texto2"'/' backends/Makefile
-#registro_error "sed"
 
 make
 registro_error $MSG_MAKE
@@ -95,7 +93,7 @@ registro_error $MSG_INST
 install -vm644 config/libelf.pc /usr/lib/pkgconfig
 registro_error "install -vm644"
 
-make check 2>&1 | tee $FILE_CHECKS
+#make check 2>&1 | tee $FILE_CHECKS
 
 ######------------------------------------------------------------------
 
@@ -106,24 +104,3 @@ rm -rf $nombre_dir && echo "Borrado el directorio $nombre_dir"
 #Registro de tiempos de ejecución
 T_FINAL=$(date +"%T")
 echo "$(date) $nombre <$MSG_TIME> $T_COMIENZO $T_FINAL" >> $FILE_BITACORA
-
-
-#error
-#In file included from m68k_corenote.c:72:
-#linux-core-note.c:116:1: error: alignment 2 of 'struct m68k_prstatus' is less than 4 [-Werror=packed-not-aligned]
-# ;
-# ^
-#linux-core-note.c:110:5: error: '({anonymous})' offset 70 in 'struct m68k_prstatus' isn't aligned to 4 [-Werror=packed-not-aligned]
-#     ;
-#     ^
-#  CC       bpf_regs.o
-#  CC       i386_init.os
-#cc1: all warnings being treated as errors
-#make[2]: *** [Makefile:750: m68k_corenote.o] Error 1
-#make[2]: *** Waiting for unfinished jobs....
-
-#/elfutils-0.170/backends/Makefile
-#linea 292 CFLAGS = -D_FORTIFY_SOURCE=2 -g -O2-Wno-error=packed-not-aligned
-
-
-
