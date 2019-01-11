69d81ea1e1cda64883ab0c58c1283b24e657d6a8
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2019-01-11 22:01:40 +0100

    Add files via upload

---
+++ 3-inst-clfs-system/ibasicsyssw117_sysklogd.sh
@@ -0,0 +1,119 @@
+# Instalador de sysklogd
+# MUSL.
+
+#sysklogd-1.5.1.tar.gz
+
+nombre=$(echo $0 | cut -d "." -f2 | cut -d "_" -f2)
+nombre_comp=$nombre-$1.tar.$2
+nombre_dir=$nombre-$1
+
+T_COMIENZO=$(date +"%T")	#para calcular el tiempo de instalación
+
+function registro_error () {
+#Función para registrar errores y resultados en la bitaćora
+if [ $? -ne 0 ]
+then
+	MOMENTO=$(date)
+	echo $MSG_ERR
+	echo "$MOMENTO $nombre <$1> -> ERROR" >> $FILE_BITACORA
+	exit 1
+else
+	MOMENTO=$(date)
+	echo "$MOMENTO $nombre <$1> -> Conforme" >> $FILE_BITACORA
+fi
+}
+
+
+# $# es el nº de argumentos. Aquí se comprueba la sintaxis de la 
+# aplicación
+if [ $# != 2 ]
+then
+	echo "uso ./nombre vesion compresion"
+	echo "nombre = nombre del programa"
+	echo "version = x.y.z"
+	echo "compresión = gz / xz etc"
+	exit 1
+fi
+
+if [ $(id -u) -ne 0 ]
+	then 
+		echo -e "Ur not root bro"
+		echo -e "Tines que ser root, tío"
+	exit 1
+fi
+
+cd $DIR_FUENTES
+
+if [ -e $nombre_comp ]
+then
+	echo "Comienza la descompresión ..."
+else
+	echo "no existe el archivo, o no tiene el formato .tar"
+	echo "se abandona la instalación"
+	exit 1
+fi
+
+case $2 in
+	gz)
+	tar -zxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	bz2)
+	tar -jxf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	xz)
+	tar -xf $nombre_comp || (echo "error descompresión"; exit 1);;
+	
+	tgz)
+	tar -xvzf $nombre_comp || (echo "error descompresión"; exit 1);;
+
+	*)
+	echo "uso ./$nombre x.y.z (gz/bz2/zx)"
+	exit 1;;
+esac	
+echo "... fin de la descompresión"
+
+cd $nombre_dir
+
+#----------------------CONFIGURE - MAKE - MAKE INSTALL------------------
+
+############# ver https://patchwork.ozlabs.org/patch/700175/ para parche
+
+echo -e "\nInstalacion de $nombre_dir MUSL" >> $FILE_BITACORA
+
+patch -Np1 -i ../sysklogd-1.5.1.patch
+registro_error "parche"
+
+sed -i '/Error loading kernel symbols/{n;n;d}' ksym_mod.c
+registro_error "sed1"
+
+make
+registro_error $MSG_MAKE
+
+make BINDIR=/sbin install
+registro_error $MSG_INST
+
+##no tiene make check 2>&1 | tee $FILE_CHECKS
+
+cat > /etc/syslog.conf << "EOF"
+# Begin /etc/syslog.conf
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+# End /etc/syslog.conf
+EOF
+registro_error "/etc/syslog.conf"
+
+
+######------------------------------------------------------------------
+
+cd ..
+rm -rf $nombre_dir && echo "Borrado el directorio $nombre_dir"
+
+
+#Registro de tiempos de ejecución
+T_FINAL=$(date +"%T")
+echo "$(date) $nombre <$MSG_TIME> $T_COMIENZO $T_FINAL" >> $FILE_BITACORA
